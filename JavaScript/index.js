998fa3480be06d5273855612c92734e690ccd269
Shahadat <shahadat.bin20@outlook.com>
2021-11-04 22:43:05 +0600

    can call function many times

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,52 +1,58 @@
-//Count the word
-let count = 0;
+function typedText(texts, className) {
 
-//count the char
-let index = 0;
+	//Count the word
+	let count = 0;
 
-let currentText = '';
-let letter = '';
-let forward = true;
-let irretation = 0;
+	//count the char
+	let index = 0;
 
-function typeText(texts, className) {
-	//after finishing a word it moves to the next one.
-	if (index < 0) {
-		count++;
-		forward = !forward;
-		index = 0;
-	}
+	let currentText = '';
+	let letter = '';
+	let forward = true;
+	let irretation = 0;
 
-    //if it went through the whole array it will start from beginning.
-	if(count == texts.length) {
-		count = 0;
-	}
-
-    //current word/sentence.
-	currentText = texts[count];
-
-
-    //if true goes forward if false goes backward.
-	if (forward) {
-		letter = currentText.slice(0, ++index);
-	}else {
-		letter = currentText.slice(0, --index);
-	}
-
-    //irretation doesn't do anything.
-	if(index == -1){
-		irretation++;
-	}else{
-		document.querySelector(className).textContent = letter;
-	}
+	function type(texts, className) {
 	
-    //if it went forward then now it will backward.
-	if(letter.length === currentText.length) {
-		forward = !forward;
+		//after finishing a word it moves to the next one.
+		if (index < 0) {
+			count++;
+			forward = !forward;
+			index = 0;
+		}
+	
+		//if it went through the whole array it will start from beginning.
+		if (count == texts.length) {
+			count = 0;
+		}
+	
+		//current word/sentence.
+		currentText = texts[count];
+	
+	
+		//if true goes forward if false goes backward.
+		if (forward) {
+			letter = currentText.slice(0, ++index);
+		} else {
+			letter = currentText.slice(0, --index);
+		}
+	
+		//irretation doesn't do anything.
+		if (index == -1) {
+			irretation++;
+		} else {
+			document.querySelector(className).textContent = letter;
+		}
+	
+		//if it went forward then now it will backward.
+		if (letter.length === currentText.length) {
+			forward = !forward;
+		}
+	
+		//how much time between each char.
+		setTimeout(type, 300, texts, className);
 	}
 
-    //how much time between each char.
-	setTimeout(typeText, 300, texts, className);
+	type(texts, className);
 }
-//export function.
-export default typeText;
+
+export default typedText;
--- JavaScript/main.js
+++ JavaScript/main.js
@@ -4,6 +4,10 @@ import typeText from "./index.js";
 
 const list = ['Great', 'Awsome', 'Strong', 'Courages'];
 
+const happy = ['Have a nice day.'];
+
 //array goes in the 1st pram & selected HTML item in the next
 
 typeText(list, '.type-text');
+
+typeText(happy, '.happy');
